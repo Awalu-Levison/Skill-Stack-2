855d6730a48a18a2578e0fc76e697bc6017827c8
Awalu-Levison <levisonawalu251@gmail.com>
2024-09-15 17:08:15 +0200

    Adding some files

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 """Flask application"""
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        username = request.form.get('password')
 
 
-if __mame__ == "__main__":
+        if username == 'skill' and password == 'password':
+            return 'Success'
+        else:
+            return 'Failure'
+
+
+#Custom filter function
+@app.template_filter('reverse_string')
+def reverse_string(s):
+    return s[::-1]
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
