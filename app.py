9f565acc158dc4a07d514af07edbe1f5a05b02c6
Awalu-Levison <levisonawalu251@gmail.com>
2024-09-15 17:27:51 +0200

    Add some files

--- app.py
+++ app.py
@@ -3,26 +3,10 @@ from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__, template_folder='templates')
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+@app.route('/home')
 def index():
-    if request.method == 'GET':
-        return render_template('index.html')
-    elif request.method == 'POST':
-        username = request.form.get('username')
-        username = request.form.get('password')
-
-
-        if username == 'skill' and password == 'password':
-            return 'Success'
-        else:
-            return 'Failure'
-
-
-#Custom filter function
-@app.template_filter('reverse_string')
-def reverse_string(s):
-    return s[::-1]
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
