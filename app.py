6a3005a40cd03dce1082b06bae295a07a587d672
Awalu-Levison <levisonawalu251@gmail.com>
2024-09-16 07:43:40 +0200

    Add some files

--- app.py
+++ app.py
@@ -5,8 +5,33 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
-def index():
+def home():
     return render_template('index.html')
+
+
+@app.route('/course')
+def course():
+    return render_template('course.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 
 if __name__ == "__main__":
